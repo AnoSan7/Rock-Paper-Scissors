6ad9ed255bfefbaddc4ac5d280fe29a710233f10
AnoSan <anomitra.santra.mat24@itbhu.ac.in>
2025-06-10 20:22:12 +0530

    Create external css and js

---
+++ script.js
@@ -0,0 +1,59 @@
+function getHumanChoice(){
+    let myChoice=prompt("Enter your choice: Rock, Paper or Scissor").toLowerCase();
+    return myChoice;
+}
+
+function getComputerChoice(){
+    let choice=0+Math.floor(Math.random()*3);
+    if(choice==0){
+        return "rock";
+    }
+    else if(choice==1){
+        return "paper";
+    }
+    else{
+        return "scissor";
+    }
+}
+
+let result=document.querySelector('#result');
+
+function playRound(myChoice, compChoice) {
+    console.log(`Your choice is ${myChoice}`);
+    console.log(`Computer's choice is ${compChoice}`);
+    if(myChoice==compChoice){
+        result.textContent="Match drawn";
+    }
+    else if((myChoice=="rock" && compChoice=="scissor") ||
+        (myChoice=="scissor" && compChoice=="paper") ||
+        (myChoice=="paper" && compChoice=="rock")){
+            result.textContent="You win this game";
+            myScore++;
+    }
+    else{
+        result.textContent="You lose this game";
+        compScore++;
+    }
+}
+
+let myScore=0, compScore=0;
+
+function playGame(){
+    for(let i=0;i<5;i++){
+        const myChoice = getHumanChoice();
+        const compChoice = getComputerChoice();
+        playRound(myChoice, compChoice);
+    }
+    if(myScore==compScore){
+        console.log("Game drawn");
+    }
+    else if(myScore>compScore){
+        console.log("You won the Game");
+    }
+    else{
+        console.log("You lost. Game Over!");
+    }
+}
+
+
+// playGame();
